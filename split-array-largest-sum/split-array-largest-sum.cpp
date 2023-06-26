ebfc30f13d08292d2d5c476c77800aefa7e44639
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-06-26 17:07:09 +0530

    Time: 6 ms (36.28%) | Memory: 7.2 MB (66.18%) - LeetSync

--- split-array-largest-sum/split-array-largest-sum.cpp
+++ split-array-largest-sum/split-array-largest-sum.cpp
@@ -19,21 +19,16 @@ public:
         for(int i=0; i<nums.size(); i++){
             e = e + nums[i];
         }
-        int ans=0;
+        
         while(s<=e){
             int mid = s + (e-s)/2;
-            if(find(nums, mid)==k){
-                ans = mid;
+            if(find(nums, mid)<=k){
                 e = mid-1;
-            }
-            else if(find(nums, mid)>k){
-                s = mid+1;
             }
             else{
-                ans = mid;
-                e = mid-1;
+                s = mid+1;
             }
         }
-        return ans;
+        return s;
     }
 };
