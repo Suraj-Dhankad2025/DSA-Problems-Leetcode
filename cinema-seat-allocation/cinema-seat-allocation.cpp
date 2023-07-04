9766f84656ecba8c4f6715f0ccc59bee66364d10
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-07-04 17:33:24 +0530

    Time: 261 ms (43.25%) | Memory: 69.7 MB (40.49%) - LeetSync

--- cinema-seat-allocation/cinema-seat-allocation.cpp
+++ cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void isPlace(vector<int>seat, bool &lplace,bool &rplace,bool &mplace){
+    void isPlace(vector<int>seat, bool &l,bool &r,bool &m){
         for(int i=0;i<seat.size();i++){
-            if(seat[i]>=1 && seat[i]<=4)lplace=false;
-            if(seat[i]>=3 && seat[i]<=6)mplace=false;
-            if(seat[i]>=5 && seat[i]<=8)rplace=false;
+            if(seat[i]>=1 && seat[i]<=4)l=false;
+            if(seat[i]>=3 && seat[i]<=6)m=false;
+            if(seat[i]>=5 && seat[i]<=8)r=false;
         }
     }
-    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        map<int,vector<int>>booked;
-        int res=0;
-        for(int i=0;i<reservedSeats.size();i++){
-            booked[reservedSeats[i][0]-1].push_back(reservedSeats[i][1]-1);
+    int maxNumberOfFamilies(int n, vector<vector<int>>& r) {
+        map<int,vector<int>>m;
+        int ans=0;
+        for(int i=0;i<r.size();i++){
+            m[r[i][0]-1].push_back(r[i][1]-1);
         }
-        int b=booked.size();
-        res=(n-b)*2;
-        for(auto it=booked.begin();it!=booked.end();it++){
-            bool lplace=true,rplace=true,mplace=true;
-            isPlace(it->second,lplace,rplace,mplace);
-            if(mplace && lplace && rplace)res+=2;
-            else if(!lplace && !rplace && mplace)res+=1;
-            else if(lplace || mplace || rplace)res+=1;
+        int b=m.size();
+        ans=(n-b)*2;
+        for(auto it=m.begin(); it!=m.end();it++){
+            bool l=1,r=1,m=1;
+            isPlace(it->second,l,r,m);
+            if(m && l && r)ans+=2;
+            else if(!l && !r && m)ans+=1;
+            else if(l || m || r)ans+=1;
         }
-        return res;     
+        return ans;     
     }
 };
