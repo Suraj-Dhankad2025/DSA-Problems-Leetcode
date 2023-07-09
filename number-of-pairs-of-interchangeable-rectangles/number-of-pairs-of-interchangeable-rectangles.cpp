816f7e430fa5ffe00caa37acc7ac2d5f41fbff91
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-07-09 19:55:54 +0530

    Time: 533 ms (82.70%) | Memory: 140.5 MB (45.79%) - LeetSync

--- number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
+++ number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -3,20 +3,15 @@ public:
     long long interchangeableRectangles(vector<vector<int>>& r) {
         long long int ans = 0;
         int n = r.size();
-        unordered_map<double, int>m;
+        unordered_map<double, long long int>m;
         for(int i=0; i<n; i++){
             double w = r[i][0];
             double h = r[i][1];
             double v = w/h;
-            m[v]++;
-        }
-        for(auto i:m){
-            if(i.second>1){
-                while(i.second!=1){
-                    i.second--;
-                    ans+=i.second;
-                }
+            if(m.find(v)!=m.end()){
+                ans+=m[v];
             }
+            m[v]++;
         }
         return ans;
     }
