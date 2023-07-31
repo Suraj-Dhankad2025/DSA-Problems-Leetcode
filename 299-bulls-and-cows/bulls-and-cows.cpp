7ecec595d804561b26b7d891e2a31a0dd976468d
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-07-31 17:05:07 +0530

    Time: 6 ms (52.07%) | Memory: 6.6 MB (61.58%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int x=0;
+        int y=0;
+        string ans="";
+        int n = secret.size();
+        unordered_map<char, int>m;
+        for(int i=0; i<n; i++){
+            if(secret[i]!=guess[i])
+            m[secret[i]]++;
+        }
+        for(int i=0; i<n; i++){
+            if(secret[i]==guess[i]){
+                x++;
+                // continue;
+                // m[guess[i]]--;
+            }
+            else if(m.find(guess[i])!=m.end() && m[guess[i]]>0){
+                y++;
+                m[guess[i]]--;
+
+            }
+        }
+        ans+=to_string(x);
+        ans+="A";
+        ans+=to_string(y);
+        ans+="B";
+        return ans;
+    }
+};
