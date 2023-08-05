c3be2cd06704e11c2e2e9dda7875612d5f6b2d42
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-08-06 00:16:38 +0530

    Time: 611 ms (57.14%) | Memory: 186.2 MB (14.29%) - LeetSync

--- 2920-minimum-seconds-to-equalize-a-circular-array/minimum-seconds-to-equalize-a-circular-array.cpp
+++ 2920-minimum-seconds-to-equalize-a-circular-array/minimum-seconds-to-equalize-a-circular-array.cpp
@@ -7,12 +7,13 @@ public:
             m[nums[i]].push_back(i);
         }
         int res=INT_MAX;
-        for(auto v:m){
+        for(auto it:m){
             int res1 = INT_MIN;
-            for(int i=0; i<v.second.size()-1; i++){
-                res1 = max(res1, (v.second[i+1] - v.second[i])/2);
+            vector<int>v = it.second;
+            for(int i=0; i<v.size()-1; i++){
+                res1 = max(res1, (v[i+1] - v[i])/2);
             }
-            res1 = max(res1, (v.second[0]+n-v.second[v.second.size()-1])/2);
+            res1=max(res1, (v[0]+n-v[v.size()-1])/2);
             res = min(res1, res);
         }
         return res;
