0f8a38998f55c9b78399fdf0f3fa9c799942f89e
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-08-05 11:31:05 +0530

    Time: 38 ms (95.90%) | Memory: 234.9 MB (15.11%) - LeetSync

---
+++ 403-frog-jump/frog-jump.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool find(int prev, int jump, vector<int>&stones, unordered_map<int,int>&m, vector<vector<int>>&dp){
+
+        int position = stones[prev] + jump;
+        if(position==stones.back()){
+            return true;
+        }
+        if(m.find(position)==m.end()){
+            return false;
+        }
+        if(dp[prev][jump]!=-1)return dp[prev][jump];
+        int ind = m[position];
+        bool ans;
+        ans = find(ind, jump+1, stones,m,dp);
+        ans = ans||find(ind, jump, stones, m,dp);
+        if(jump>1){
+            ans = ans||find(ind, jump-1, stones, m,dp);
+        }
+        return dp[prev][jump] = ans;
+    }
+    bool canCross(vector<int>& stones) {
+        unordered_map<int, int>m;
+        vector<vector<int>>dp(stones.size(), vector<int>(stones.size(),-1));
+        for(int i=0; i<stones.size(); i++){
+            m[stones[i]] = i;
+        }
+        return find(0,1,stones,m,dp);
+    }
+};
