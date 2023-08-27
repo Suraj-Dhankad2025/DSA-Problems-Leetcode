25ac2db4f3c1a9b8771d34fa854562ecf94f5d60
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-08-27 20:10:24 +0530

    Time: 14 ms (51.79%) | Memory: 10.1 MB (21.68%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& v, int k) {
-        sort(v.begin(),v.end());
-        int n = v.size();
-        int mn = 0, mx = v[n-1]-v[0]+1,mid,ans,x,i,j;
-        while(mn <= mx){
-            mid = (mx-mn)/2+mn;
-            i = 0, j = 0,x=0;
-            while(i<n){
-                while(v[i]-v[j]>mid){
-                    j++;
-                }
-                x += (i-j);
-                i++;
+    int rank(vector<int>&nums,int diff){
+        int count=0;
+        int i=0;
+        int j=1;
+        while(i<nums.size()){
+            while(j<nums.size()&&nums[j]-nums[i]<=diff){
+                j++;
             }
-            if(x>=k){
-                ans = mid;
-                mx = mid-1;
-            }else{
-                mn = mid+1;
+            j--;
+            count+=j-i;
+            i++;
+        }
+        return count;
+    }
+
+    int smallestDistancePair(vector<int>& nums, int k) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int low=0;
+        int high=nums[n-1]-nums[0];
+        int ans=0;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            int x=rank(nums,mid);
+            if(x<k){
+                low=mid+1;
+            }
+            else{
+                high=mid;
             }
         }
-        return ans;
+        return low;
     }
 };
