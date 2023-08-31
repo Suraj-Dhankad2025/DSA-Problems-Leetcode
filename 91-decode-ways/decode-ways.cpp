a127326390e738cf6c354e25a381920839f326fc
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-08-31 13:51:08 +0530

    Time: 4 ms (31.94%) | Memory: 8.2 MB (10.11%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int find(int i, string s, vector<int>&dp){
+        if(i<0){
+            return 1;
+        }
+        if(dp[i]!=-1)return dp[i];
+        int one = 0;
+        if(s[i]!='0'){
+            one = find(i-1, s, dp);
+        }
+        int two = 0;
+        if(i-1>=0 && s[i-1]!='0'){
+            int x = (s[i-1]-'0')*10 + (s[i]-'0');
+            cout<<x<<" ";
+            if(x<=26){
+                two = find(i-2,s,dp);
+            }
+        }
+        return dp[i] = one + two;
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.size(), -1);
+        return find(s.size()-1,s,dp);
+    }
+};
