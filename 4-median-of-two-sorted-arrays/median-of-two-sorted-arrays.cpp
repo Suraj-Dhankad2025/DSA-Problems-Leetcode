713e819d7af21d6b3e577a0ce1a5cefc7f08dfbe
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-08-31 21:11:59 +0530

    Time: 23 ms (84.91%) | Memory: 89.3 MB (77.37%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,59 +3,42 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int n = n1 + n2;
-        int i=0,j=0;
-        int ind2=n/2;
-        int ind1=ind2 -1;
-        int a=-1,b=-1;
-        int ind=0;
-        while(i<n1 && j<n2){
-            if(nums1[i]<nums2[j]){
-                if(ind==ind1){
-                    a = nums1[i];
+        if(n1>n2){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0,high = n1;
+        int n = n1+n2;
+        int left = (n1+n2+1)/2;
+        while(low<=high){
+            int mid1 = (low+high)/2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, r1 = INT_MAX;
+            int l2 = INT_MIN, r2 = INT_MAX;
+            if(mid1<n1){
+                r1 = nums1[mid1];
+            }
+            if(mid2<n2){
+                r2 = nums2[mid2];
+            }
+            if(mid1-1>=0){
+                l1 = nums1[mid1-1];
+            }
+            if(mid2-1>=0){
+                l2 = nums2[mid2-1];
+            }
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1){
+                    return max(l1, l2);
                 }
-                if(ind==ind2){
-                    b = nums1[i];
-                }
-                i++;
-                ind++;
+                return ((double)(max(l1, l2) + min(r1, r2)))/2.0;
+            }
+            else if(l1>r2){
+                high = mid1 - 1;
             }
             else{
-                if(ind==ind1){
-                    a = nums2[j];
-                }
-                if(ind==ind2){
-                    b = nums2[j];
-                }
-                j++;
-                ind++;
+                low = mid1 + 1;
             }
         }
-        while(i<n1){
-            if(ind==ind1){
-                a = nums1[i];
-            }
-            if(ind==ind2){
-                b = nums1[i];
-            }
-            i++;
-            ind++;
-        }
-        while(j<n2){
-            if(ind==ind1){
-                a = nums2[j];
-            }
-            if(ind==ind2){
-                b = nums2[j];
-            }
-            j++;
-            ind++;
-        }
-        if(n%2==1){
-            return b;
-        }
-        else{
-            return (double)(double)(a+b)/2.0;
-        }
+        return 0;
     }
 };
