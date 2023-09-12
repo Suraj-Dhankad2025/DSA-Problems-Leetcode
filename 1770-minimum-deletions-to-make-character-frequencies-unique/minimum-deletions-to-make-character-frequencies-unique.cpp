f7a2d0c61b335932dcdfcc612af03c353be666c7
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-09-13 01:09:57 +0530

    Time: 73 ms (43.95%) | Memory: 17.5 MB (38.25%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -7,18 +7,13 @@ public:
             m[s[i]]++;
         }
         set<int>st;
-        // for(auto i:m){
-        //     st.insert(i.second);
-        // }
         for(auto i:m){
             int f=i.second;
-            while(st.find(f)!=st.end()){
+            while(st.find(f)!=st.end() && f>0){
                 f--;
                 ans++;
             }
-            if(f>0){
-                st.insert(f);
-            }
+            st.insert(f);
         }
         return ans;
     }
