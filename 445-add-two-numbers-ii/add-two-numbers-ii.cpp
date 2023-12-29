caf35835b57773e459890ee8609dbe4db960b269
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-12-29 16:40:55 +0530

    Time: 27 ms (39.76%) | Memory: 71.7 MB (47.95%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -11,51 +11,55 @@
 class Solution {
 public:
     ListNode* rev(ListNode* head){
-        ListNode* prev = NULL, *cur = head, *nxt = NULL;
-        while(cur!=NULL){
-            nxt = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = nxt;
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        while(curr!=nullptr){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
         }
         return prev;
     }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* h1 = rev(l1);
-        ListNode* h2 = rev(l2);
-        ListNode* temp1 = h1;
-        ListNode* temp2 = h2;
+        ListNode* temp = new ListNode(-1);
+        ListNode* list1 = rev(l1);
+        ListNode* list2 = rev(l2);
         int carry = 0;
-        ListNode* newHead = new ListNode(-1);
-        ListNode* tempHead = newHead;
-        while(temp1!=NULL && temp2!=NULL){
-            int num = temp1->val + temp2->val + carry;
-            ListNode* node = new ListNode(num%10);
+        ListNode* temp1 = temp;
+        int num = 0;
+        while(list1!=NULL || list2!=NULL){
+            if(list1!=nullptr && list2!=nullptr){
+                num = list1->val+list2->val + carry;
+            }
+            else if(list1==nullptr && list2!=nullptr){
+                num = list2->val + carry;
+            }
+            else {
+                num = list1->val+ carry;
+            }
+
+            ListNode* newl = new ListNode(num%10);
+            temp1->next = newl;
+            temp1 = temp1->next;
             carry = num/10;
-            tempHead->next = node;
-            tempHead = tempHead->next;
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-        while(temp1!=NULL){
-            ListNode* node = new ListNode((temp1->val+carry)%10);
-            carry = (temp1->val+carry)/10;
-            tempHead->next = node;
-            tempHead = tempHead->next;
-            temp1 = temp1->next;
-        }
-        while(temp2!=NULL){
-            ListNode* node = new ListNode((temp2->val+carry)%10);
-            carry = (temp2->val+carry)/10;
-            tempHead->next = node;
-            tempHead = tempHead->next;
-            temp2 = temp2->next;
+            if(list1)
+                list1 = list1->next;
+            if(list2)
+                list2 = list2->next;
+
         }
         if(carry!=0){
-            ListNode* node = new ListNode(carry);
-            tempHead->next = node;
-            tempHead = tempHead->next;
+            ListNode* temp2 = new ListNode(carry);
+            temp1->next = temp2;
+            temp1 = temp2;
         }
-        return rev(newHead->next);
+        temp = temp->next;
+        temp = rev(temp);
+        return temp;
     }
 };
