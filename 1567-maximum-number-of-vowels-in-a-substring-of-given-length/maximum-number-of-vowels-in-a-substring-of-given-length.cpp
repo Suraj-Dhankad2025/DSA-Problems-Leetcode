03d667a2e65dafcae8ad0bec6b67e0d8867f69f6
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-12-29 21:32:15 +0530

    Time: 27 ms (28.90%) | Memory: 10.3 MB (75.24%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        map<char, int>m;
-        m['a']++;
-        m['e']++;
-        m['i']++;
-        m['o']++;
-        m['u']++;
         int n = s.size();
         int i=0,j=0;
         int c=0;
         int ans=0;
         while(j<n){
-            if(m.find(s[j])!=m.end()){
+            int ch = s[j];
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
                 c++;
             }
             while(j-i+1>k){
-                if(m.find(s[i])!=m.end()){
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
                     c--;
                 }
                 i++;
