49d82cb00ec80d694d6baee7494d25466c9772c5
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-01 19:02:30 +0530

    Time: 179 ms (5.07%) | Memory: 45.7 MB (53.32%) - LeetSync

---
+++ 3202-high-access-employees/high-access-employees.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool find(int a, int b){
+        int n = min(a, b);
+        int m = max(a, b);
+        if(m>2300 && n<100){
+            n += (2400-m);
+            if(n<100)return 1;
+            return 0;
+        }
+        if(abs(a-b)<100)return 1;
+        return 0;
+    }
+    vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
+        vector<string>ans;
+        int n = access_times.size();
+        unordered_map<string, vector<int>>m;
+        for(int i=0; i<n; i++){
+            m[access_times[i][0]].push_back(stoi(access_times[i][1]));
+        }
+        for(auto it:m){
+            vector<int>v = it.second;
+            sort(v.begin(), v.end());
+            int i=0,j=2;
+            while(j<v.size()){
+                if(find(v[i], v[j])==1){
+                    ans.push_back(it.first);
+                    break;
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
