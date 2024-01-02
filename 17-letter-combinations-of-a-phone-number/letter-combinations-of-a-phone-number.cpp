54232775ffa767545d216623d243433ce970c9d7
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-02 15:33:23 +0530

    Time: 2 ms (42.84%) | Memory: 7.4 MB (7.90%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     vector<string>ans;
-    void find(int i,string temp, string s, vector<string>&v){
+    void find(int i,string s, string temp, map<int, vector<char>>&m){
         if(i==s.size()){
             ans.push_back(temp);
             return ;
         }
-        for(auto it:v[s[i]-'0'-2]){
+        for(auto it:m[(s[i])-'0']){
             temp.push_back(it);
-            find(i+1, temp, s, v);
+            find(i+1,s,temp,m);
             temp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string>v(8);
         int n = digits.size();
         if(n==0)return {};
-        v[0] = "abc";
-        v[1] = "def";
-        v[2] = "ghi";
-        v[3] = "jkl";
-        v[4] = "mno";
-        v[5] = "pqrs";
-        v[6] = "tuv";
-        v[7] = "wxyz";
-        find(0, "", digits, v);
+        map<int, vector<char>>m;
+        char ch='a';
+        int cnt=0;
+        int i=2;
+        while(i<=9){
+            m[i].push_back(ch);
+            ch++;
+            cnt++;
+            if(i!=7 && i!=9 && cnt==3){
+                cnt = 0;
+                i++;
+            }
+            if(i==7 || i==9){
+                if(cnt==4){
+                    cnt = 0;
+                    i++;
+                }
+            }
+        }
+        find(0, digits, "", m);
         return ans;
     }
 };
