8b7ac3bcad1b13e243651a6f2ce1ea2f2485281d
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-02 14:37:38 +0530

    Time: 7 ms (6.18%) | Memory: 6.9 MB (70.02%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     vector<string>ans;
-    void find(int i,string temp, string s, map<int, string>&m){
+    void find(int i,string temp, string s, vector<string>&v){
         if(i==s.size()){
             ans.push_back(temp);
             return ;
         }
-        for(auto it:m[s[i]-'0']){
+        for(auto it:v[s[i]-'0'-2]){
             temp.push_back(it);
-            find(i+1, temp, s, m);
+            find(i+1, temp, s, v);
             temp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        map<int, string>m;
+        vector<string>v(8);
         int n = digits.size();
         if(n==0)return {};
-        m[2] = "abc";
-        m[3] = "def";
-        m[4] = "ghi";
-        m[5] = "jkl";
-        m[6] = "mno";
-        m[7] = "pqrs";
-        m[8] = "tuv";
-        m[9] = "wxyz";
-        find(0, "", digits, m);
+        v[0] = "abc";
+        v[1] = "def";
+        v[2] = "ghi";
+        v[3] = "jkl";
+        v[4] = "mno";
+        v[5] = "pqrs";
+        v[6] = "tuv";
+        v[7] = "wxyz";
+        find(0, "", digits, v);
         return ans;
     }
 };
