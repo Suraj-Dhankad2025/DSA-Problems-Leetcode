01ea220af319fc37bc7dcb18610177342b778b46
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-11 23:07:21 +0530

    Time: 1300 ms (9.16%) | Memory: 165.4 MB (19.03%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
-    long long find(vector<vector<pair<int, int>>>&g, int s, int t){
+    int find(vector<pair<int, int>>adj[], int s, int t){
         queue<pair<int, int>>q;
-        long long res=LONG_MAX;
-        vector<long long> pathCost(26, LONG_MAX);
-        pathCost[s] = 0;
-        q.push({s,0});
+        q.push({0,s});
+        vector<long long>cost(26,LONG_MAX);
+        cost[s] = 0;
+        long long ans=LONG_MAX;
         while(!q.empty()){
-            auto [node, cost] = q.front();
+            int c = q.front().first;
+            int ch = q.front().second;
             q.pop();
-            for(auto [n,c]:g[node]){
-                if(cost+c>=pathCost[n])continue;
-                q.push({n, cost+c});
-                pathCost[n] = cost + c;
-                if(n==t){
-                    res = cost + c;
+            for(auto it:adj[ch]){
+                int newCh = it.first;
+                int newCost = it.second;
+                if(newCost+c<cost[newCh]){
+                    q.push({newCost+c, newCh});
+                    cost[newCh] = newCost + c;
+                    if(newCh==t){
+                        ans = newCost + c;
+                    }
                 }
             }
         }
-        return (res==LONG_MAX)? -1:res;
+        return (ans==LONG_MAX)? -1:ans;
     }
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        long long ans=0;
-        int n = cost.size();
-        vector<vector<pair<int, int>>>graph(26);
-        for(int i=0; i<n; i++){
-            graph[original[i]-'a'].push_back({changed[i]-'a', cost[i]});
+        vector<pair<int, int>>adj[26];
+        for(int i=0; i<original.size(); i++){
+            adj[original[i]-'a'].push_back({changed[i]-'a', cost[i]});
         }
-
-        unordered_map<long long, long long>m;
-
-        for(int i=0; i<source.size(); i++){
-            if(source[i]==target[i]){
-                continue;
+        int n = source.size();
+        long long ans = 0;
+        unordered_map<long long, long long>mp;
+        for(int i=0; i<n; i++){
+            if(source[i]!=target[i]){
+                long long temp = -1;
+                long long hash = (source[i]-'a')*100 + target[i] - 'a';
+                if(mp.find(hash)!=mp.end()){
+                    temp = mp[hash];
+                }
+                else{
+                    temp = find(adj, source[i]-'a', target[i]-'a');
+                    mp[hash] = temp;
+                }
+                if(temp==-1){
+                    return -1;
+                }
+                ans+=temp;
             }
-            long long tempCost = -1;
-            long long hash = (source[i]-'a')*100 + target[i] - 'a';
-            if(m.find(hash)!=m.end()){
-                tempCost = m[hash];
-            }
-            else{
-                tempCost = find(graph, source[i]-'a', target[i]-'a');
-                m[hash] = tempCost;
-            }
-            
-            if(tempCost==-1)return -1;
-            ans+=tempCost;
         }
         return ans;
     }
