f00a0e7fd71346aca0694e4e5a134c73fde61b55
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-13 19:28:26 +0530

    Time: 4 ms (85.64%) | Memory: 8 MB (57.31%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int equalSubstring(string s, string t, int cost) {
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
         int i=0,j=0;
-        int ans=0;
-        while(j<s.size()){
+        int ans = 0;
+        while(j<n){
             if(s[j]==t[j]){
                 ans = max(ans, j-i+1);
                 j++;
                 continue;
             }
-            cost -= abs(s[j]-t[j]);
-            while(cost<0 && i<j){
-                cost += abs(s[i]-t[i]);
+            maxCost = maxCost - abs(s[j]-t[j]);
+            while(maxCost<0 && i<j){
+                maxCost = maxCost + abs(s[i]-t[i]);
                 i++;
             }
-            if(cost>=0)ans = max(ans, j-i+1);
-            // cout<<i<<" "<<j<<endl;
-            // cout<<ans<<endl;
+            if(maxCost>=0)ans = max(ans, j-i+1);
             j++;
         }
         return ans;
