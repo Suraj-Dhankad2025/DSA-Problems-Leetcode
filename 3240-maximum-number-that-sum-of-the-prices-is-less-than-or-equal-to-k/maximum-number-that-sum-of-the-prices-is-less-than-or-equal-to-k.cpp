d2b149c8fcfc975769e1e7a6ab4328f5e52fde3f
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-14 13:33:57 +0530

    Time: 10 ms (66.67%) | Memory: 6.8 MB (33.33%) - LeetSync

--- 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
+++ 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    long long find(long long mid, int x){
-        long long sum=0;
-        for(long long i=x-1;i<=60;i+=x){
-            long long dividend=mid+1;
-            long long patsize=(1LL<<(i+1));
-            sum+=patsize/2*(dividend/patsize);
-            long long rem=dividend%patsize;
-            if(rem<=patsize/2) continue;
-            rem%=(patsize/2);
-            sum+=rem;
-        }
-        return sum;
-    }
     long long findMaximumNumber(long long k, int x) {
-        long long ans=1;
-        long long start = 1;
-        long long end = 1e15-1;
-        while(start<=end){
-            long long mid = start + (end-start)/2;
-            long long sum = find(mid,x);
-            if(sum>k){
-                end = mid-1;
+        long long ans;
+        long long l = 0, r = 1e17;
+        vector<int> v;    //bits which should be considered
+        for (int i = 0; i < 54;i++){
+            if(((i+1)%x)==0)
+                v.push_back(i);
+        }
+        while (l <= r)
+        {
+            long long m = (l + r) / 2;
+            long long count = 0;
+            long long one=1;
+            for(int i: v){
+                count += ((m + 1) / (one<<(i+1)))*(one<<i);   //in ith bit 2^i 0s and 1s repeat
+                long long t=(m+1)/(one<<i);     // number of one set 0 and 1
+
+                if(t&1){                      //t is odd
+                    count += (m+1)%(one<<i);  //then all 0s are covered and at last few 1s may be present add these 1s
+                }
+
+            }
+
+            if(count<=k){
+                ans = m;
+                l = m + 1;
             }
             else{
-                ans = max(ans, mid);
-                start = mid+1;
+                r = m - 1;
             }
         }
         return ans;
-    }
-   
+        }
 };
