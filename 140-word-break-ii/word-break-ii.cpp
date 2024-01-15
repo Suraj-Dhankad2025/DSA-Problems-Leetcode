fa785262c436f3b307d3403af8a53fca61efc6e5
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-15 20:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.41%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<string>ans;
-    set<string>st;
-    void make(int ind,string &s, string temp){
-        if(ind>=s.size()){
-            string t = temp.substr(0,temp.length()-1);
-            ans.push_back(t);
+    void find(int i, string temp, string &s, set<string>&st, vector<string>&ans){
+        if(i>=s.size()){
+            temp.pop_back();
+            ans.push_back(temp);
             return ;
         }
-        for(int i=ind; i<s.length(); i++){
-            if(!st.count(s.substr(ind,i+1-ind))){
-                continue;
-            } 
-            make(i+1, s, temp+(s.substr(ind, i+1-ind))+" ");
+        for(int j=i; j<s.size(); j++){
+            if(st.find(s.substr(i, j-i+1))!=st.end()){
+                find(j+1, temp+s.substr(i, j-i+1)+" ", s, st, ans);
+            }
         }
         
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string>ans;
+        set<string>st;
         for(auto i:wordDict){
             st.insert(i);
         }
-        make(0, s, "");
+        find(0, "", s, st, ans);
         return ans;
     }
 };
