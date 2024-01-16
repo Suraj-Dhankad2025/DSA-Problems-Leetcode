ad5489592150245d51d5f2417b62ae6632de80e7
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-17 03:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (26.88%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,16 +1,50 @@
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int ans=0;
-        string s = a;
-        if(a == b) return 1;
-        while(s.length()<=10000){
-            if(s.find(b)!=-1){
-                return ans+1;
+    void findlps(string s, vector<int>&lps){
+        int pre = 0, suff = 1;
+        while(suff < s.size()){
+            if(s[pre]==s[suff]){
+                lps[suff] = pre+1;
+                suff++; pre++;
             }
-            s+=a;
-            ans++;
+            else {
+                if(pre==0) suff++;
+                else {
+                    pre = lps[pre-1];
+                }
+            }
         }
+    }
+    int KMP_MATCH(string main, string find){
+        vector<int>lps(find.size(), 0);
+        findlps(find, lps);
+
+        int pre = 0, suff = 0;
+        while(pre < main.size() && suff < find.size()){
+            if(main[pre]==find[suff]){
+                pre++; suff++;
+            }
+            else {
+                if(suff==0) pre++;
+                else suff = lps[suff-1];
+            }
+        }
+        if(suff==find.size()) return 1;
+        else return 0;
+    }
+    int repeatedStringMatch(string a, string b) {
+        if(a==b) return 1;
+
+        int repeatation = 1;
+        string newstr = a;
+        while(newstr.size() < b.size()){
+            newstr+=a;
+            repeatation++;
+        }
+
+        // searching using kmp 
+        if(KMP_MATCH(newstr, b)==1) return repeatation;
+        if(KMP_MATCH(newstr+a, b)==1) return repeatation+1;
         return -1;
     }
 };
