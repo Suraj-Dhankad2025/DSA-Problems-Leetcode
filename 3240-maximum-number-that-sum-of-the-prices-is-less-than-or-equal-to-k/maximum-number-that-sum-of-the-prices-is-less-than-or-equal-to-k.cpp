66e88e79efa1e2ad4fc0d3b2afd63759874d7603
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-17 03:05:54 +0530

    Time: 38 ms (20.41%) | Memory: 6.4 MB (41.32%) - LeetSync

--- 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
+++ 3240-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.cpp
@@ -1,36 +1,49 @@
+#define ll long long
+ll bitSum[100];
 class Solution {
 public:
-    long long findMaximumNumber(long long k, int x) {
-        long long ans;
-        long long l = 0, r = 1e17;
-        vector<int> v;    //bits which should be considered
-        for (int i = 0; i < 54;i++){
-            if(((i+1)%x)==0)
-                v.push_back(i);
+    void findBitSum(ll n){
+        if(n==0) return;
+        if(n==1){
+            bitSum[0]++;
+            return;
         }
-        while (l <= r)
-        {
-            long long m = (l + r) / 2;
-            long long count = 0;
-            long long one=1;
-            for(int i: v){
-                count += ((m + 1) / (one<<(i+1)))*(one<<i);   //in ith bit 2^i 0s and 1s repeat
-                long long t=(m+1)/(one<<i);     // number of one set 0 and 1
+        if(n==2){
+            bitSum[0]++;
+            bitSum[1]++;
+            return;
+        }
 
-                if(t&1){                      //t is odd
-                    count += (m+1)%(one<<i);  //then all 0s are covered and at last few 1s may be present add these 1s
+        ll mostSignificantBit = log2(n);
+        ll cn = (1ll<<mostSignificantBit);
+        bitSum[mostSignificantBit] += n-cn+1;
+        for(ll i=0; i<mostSignificantBit; i++){
+            bitSum[i] += cn/2;
+        } 
+        findBitSum(n-cn);
+    }
+    long long findMaximumNumber(long long k, int x) {
+        ll s = 0;
+        ll e = 1e15;
+        ll mid = 0, ans=0;
+        while(s<=e){
+            mid = s + (e-s)/2;
+            memset(bitSum, 0, sizeof(bitSum));
+            findBitSum(mid);
+            ll sum=0;
+            for(int i=0; i<53; i++){
+                if((i+1)%x==0){
+                    sum+=bitSum[i];
                 }
-
             }
-
-            if(count<=k){
-                ans = m;
-                l = m + 1;
+            if(sum<=k){
+                ans = mid;
+                s = mid + 1;
             }
             else{
-                r = m - 1;
+                e = mid - 1;
             }
         }
         return ans;
-        }
+    }
 };
