9f20d6d7f0cbbc5501cfaf82533088ef69d2a02e
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-20 00:01:08 +0530

    Time: 145 ms (97.42%) | Memory: 77.4 MB (53.61%) - LeetSync

--- 2848-special-permutations/special-permutations.cpp
+++ 2848-special-permutations/special-permutations.cpp
@@ -1,66 +1,28 @@
-
-// const int MOD = 1e9 + 7;
-// class Solution {
-// public:
-//     int find(const vector<int>& nums, int prev, int index, int mask, vector<vector<int>>& dp) {
-//         if (index == nums.size()) {
-//             return 1;
-//         }
-//         if (dp[prev + 1][mask] != -1) {
-//             return dp[prev + 1][mask];
-//         }
-//         int total = 0;
-//         for (int i = 0; i < nums.size(); ++i) {
-//             if (!(mask & (1 << i)) && (prev == -1 || nums[i] % nums[prev] == 0 || nums[prev] % nums[i] == 0)) {
-//                 total = (total + find(nums, i, index + 1, mask | (1 << i), dp)) % MOD;
-//             }
-//         }
-//         dp[prev + 1][mask] = total;
-//         return total;
-//     }
-//     int specialPerm(const vector<int>& nums) {
-//         vector<vector<int>> dp(nums.size() + 1, vector<int>((1 << nums.size()), -1));
-//         return find(nums, -1, 0, 0, dp);
-//     }
-// };
 class Solution {
-    int n;
-    vector<vector<int>> graph;
-    vector<vector<int>> record;
-    int Mod=1e9+7;
 public:
-    int specialPerm(vector<int>& nums) {
-        n=nums.size();
-        graph.resize(n);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]%nums[j]==0||nums[j]%nums[i]==0){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
-            }
-        }
-        record.resize(n,vector<int>(1<<n,-1));
-        int ret=0;
-        for(int i=0;i<n;i++){
-            ret+=dfs(0,i,1<<i);
-            ret%=Mod;
-        }
-        return ret;
-    }
-    int dfs(int cnt, int cur, int state){
-        if(cnt==n-1)
+    int mod=1e9+7;
+    int find(int prev, int ind, vector<int>&nums,int mask, vector<vector<int>>&dp){
+        if(ind==nums.size()){
             return 1;
-        if(record[cur][state]!=-1)
-            return record[cur][state];
-        int ret=0;
-        for(int nxt:graph[cur]){
-            if((state>>nxt)&1)
-                continue;
-            ret+=dfs(cnt+1,nxt,state+(1<<nxt));
-            ret%=Mod;
         }
-        record[cur][state]=ret;
-        return ret;
+        if(dp[prev][mask]!=-1)return dp[prev][mask];
+        int ans=0;
+        for(int i=0; i<nums.size(); i++){
+            if(mask & 1<<i) continue;
+            if(mask!=0 && nums[prev]%nums[i]!=0 && nums[i]%nums[prev]!=0){
+                continue;
+            }   
+            
+            ans= (ans + find(i, ind+1, nums, mask | 1<<i, dp))%mod;
+            
+        }
+        return dp[prev][mask] = ans%mod;
+    }
+    int specialPerm(vector<int>& nums) {
+        int prev = 0;
+        int n = nums.size();
+        vector<vector<int>>dp(nums.size(), vector<int>(1<<(n), -1));
+        int mask = 0;
+        return find(prev, 0, nums, mask, dp);
     }
 };
