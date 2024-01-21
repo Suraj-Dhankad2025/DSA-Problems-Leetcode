2ce3e995c1967035e2de45232cc04394afeb9e03
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-21 21:22:17 +0530

    Time: 489 ms (60.89%) | Memory: 166.6 MB (58.61%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>>& m) {
-         vector<vector<int>>ans(2);
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<vector<int>>ans(2);
+        int n = matches.size();
         map<int, int>m1;
-        
-        int n = m.size();
-        for(int i=0; i<n; i++){
-            m1[m[i][1]]++;
-        }
         map<int, int>m2;
         for(int i=0; i<n; i++){
-            if(m1.find(m[i][0])==m1.end() && m2[m[i][0]]==0){
-                ans[0].push_back(m[i][0]);
-                m2[m[i][0]]++;
+            m1[matches[i][1]]++;
+        }
+        for(int i=0; i<n; i++){
+            if(m1.find(matches[i][0])==m1.end() && m2[matches[i][0]]==0){
+                ans[0].push_back(matches[i][0]);
+                m2[matches[i][0]]++;
             }
         }
+        
         for(auto i:m1){
             if(i.second==1){
                 ans[1].push_back(i.first);
