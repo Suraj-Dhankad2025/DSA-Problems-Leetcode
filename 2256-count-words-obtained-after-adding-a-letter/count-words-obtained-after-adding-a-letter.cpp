047605082e837c49ce28d6486fc038d74ef88a96
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-23 22:10:01 +0530

    Time: 393 ms (57.14%) | Memory: 117.6 MB (32.00%) - LeetSync

--- 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.cpp
+++ 2256-count-words-obtained-after-adding-a-letter/count-words-obtained-after-adding-a-letter.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int wordCount(vector<string>& startWords, vector<string>& targetWords) {
-        unordered_set<int> vis;
-        for (string s : startWords) {
-            int hash = 0;
-            for (int i = 0; i < s.size(); i++) {
-                hash += (1 << (s[i] - 'a'));
-            }
-            vis.insert(hash);
+        unordered_set<string> s;
+        for(auto it:startWords){
+            sort(it.begin(), it.end());
+            s.insert(it);
         }
-        int res = 0;
-        for (string s : targetWords) {
-            int hash = 0;
-            for (int k = 0; k < s.size(); k++) {
-                hash = 0;
-                for (int i = 0; i < s.size(); i++) {
-                    if (i == k) continue;
-                    hash += (1 << (s[i] - 'a'));
-                }
-                if (vis.count(hash)) {
-                    res++;
+        int ans = 0;
+        for(auto it:targetWords){
+            string st = it;
+            sort(st.begin(), st.end());
+            for(int i = 0; i < st.size(); i++){
+                string search = st.substr(0, i) + st.substr(i+1);                    
+                if(s.find(search) != s.end()){
+                    ans++;
                     break;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
