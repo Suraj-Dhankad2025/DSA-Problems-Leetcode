498fd444d2607b7f1924e087cdcd2c755d05a1df
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-27 13:04:16 +0530

    Time: 1918 ms (9.14%) | Memory: 489.3 MB (5.13%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    static bool cmp(vector<int>&a, vector<int>&b){
+    static bool cmp(vector<int>a, vector<int>b){
         if(a[0]==b[0]){
             return a[1]>b[1];
         }
         return a[0]<b[0];
     }
-    int maxEnvelopes(vector<vector<int>>& a) {
-    //     sort(envelopes.begin(), envelopes.end());
-    //     vector<int>dp(envelopes.size(), 1);
-    //     int ans = 1;
-    //     for(int i=0; i<envelopes.size(); i++){
-    //         for(int j=0; j<i; j++){
-    //             if(envelopes[i][0]>envelopes[j][0] && envelopes[i][1]>envelopes[j][1]){
-    //                 dp[i] = max(dp[i], dp[j]+1);
-    //             }
-    //         }
-    //         ans = max(ans, dp[i]);
-    //     }
-    //     return ans;
-        sort(a.begin(), a.end(), cmp);
+    int maxEnvelopes(vector<vector<int>>& e) {
+        sort(e.begin(), e.end(), cmp);
         vector<int>dp;
-        for(int i=0; i<a.size(); i++){
-            int ele = a[i][1];
-            int idx = lower_bound(dp.begin(), dp.end(), ele) - dp.begin();
-            if(idx>=dp.size()){
-                dp.push_back(ele);
+        for(int i=0; i<e.size(); i++){
+            int num = e[i][1];
+            int ind = lower_bound(dp.begin(), dp.end(), num) -  dp.begin();
+            if(ind>=dp.size()){
+                dp.push_back(num);
             }
             else{
-                dp[idx] = ele;
+                dp[ind] = num;
             }
         }
         return dp.size();
