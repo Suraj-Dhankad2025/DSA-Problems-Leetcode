07c8d1ed0025d264a261b32cdb75706dee88c87c
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-01-29 23:13:46 +0530

    Time: 7 ms (9.15%) | Memory: 8.5 MB (12.94%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.cpp
+++ 1185-find-in-mountain-array/find-in-mountain-array.cpp
@@ -10,26 +10,51 @@
 
 class Solution {
 public:
-     int length;
-    int find(int low,int high,int target,MountainArray &mountainArr){
-        if(low>high) return -1;
-        int mid,currentEle;
-        mid=low+(high-low)/2;
-        currentEle=mountainArr.get(mid);
-        if(currentEle < target){
-            if(mid!=length && currentEle<mountainArr.get(mid+1)) return find(mid+1,high,target,mountainArr);
-            else return find(low,mid-1,target,mountainArr);
+    int ans = -1;
+    int find(int target, MountainArray &a, int s, int e, bool increasingOrder) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            int ele = a.get(mid);
+            if (ele == target) {
+                if (ans == -1) {
+                    ans = mid;
+                }
+                ans = min(ans, mid);
+            }
+
+            if (increasingOrder) {
+                if (ele < target) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            } else {
+                if (ele > target) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
         }
-        else{
-            int left=find(low,mid-1,target,mountainArr);
-            return left != -1 ? left  : currentEle == target ? mid : find(mid+1,high,target,mountainArr);
-        }
-        return -1;
+        return ans;
     }
-    
+
     int findInMountainArray(int target, MountainArray &mountainArr) {
-        int low=0,high=mountainArr.length()-1;
-        length=high;
-        return find(low,high,target,mountainArr);
+        int n = mountainArr.length();
+        int s = 0, e = n - 1;
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+        ans = find(target, mountainArr, 0, e, true);
+        if (ans != -1) {
+            return ans;
+        }
+        ans = find(target, mountainArr, e + 1, n - 1, false);
+        return ans;
     }
 };
