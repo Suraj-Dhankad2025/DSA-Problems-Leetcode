440cbd407fc1ca305c4ff7009214295a76a2c0a2
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-02-10 11:01:59 +0530

    Time: 2 ms (86.45%) | Memory: 7.4 MB (74.15%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int check(string s){
-        int i=0,j=s.size()-1;
-        while(i<j){
-            if(s[i]^s[j]){
-                return 0;
-            }
-            i++;
-            j--;
+    int palin(int i, int j,string &s){
+        int cnt = 0;
+        while(i >= 0 && j < s.length() && s[i] == s[j]){
+            cnt++;
+            i--;
+            j++;
         }
-        return 1;
+        return cnt;
     }
     int countSubstrings(string s) {
-        int n = s.size();
-        int ans=0;
-        for(int i=0; i<n; i++){
-            for(int j=i ;j<n; j++){
-                if(check(s.substr(i, j-i+1)) == 1){
-                    ans++;
-                }
-            }
+        int cnt = 0;
+        for(int i = 0; i < s.length() ;i++){
+           int j = i;
+           int oddLengthVale = palin(i,j , s);
+           int evenLengthVale= palin(i , j+1 ,s);
+           cnt += (oddLengthVale + evenLengthVale);
         }
-        return ans;
+        return cnt;
+        
     }
 };
