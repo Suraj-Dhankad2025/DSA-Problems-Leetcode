ed54cd84bddecd79222d5a1e7d6fd54526392ca3
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-02-10 11:01:45 +0530

    Time: 424 ms (11.04%) | Memory: 458.5 MB (5.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -3,7 +3,7 @@ public:
     int check(string s){
         int i=0,j=s.size()-1;
         while(i<j){
-            if(s[i]!=s[j]){
+            if(s[i]^s[j]){
                 return 0;
             }
             i++;
