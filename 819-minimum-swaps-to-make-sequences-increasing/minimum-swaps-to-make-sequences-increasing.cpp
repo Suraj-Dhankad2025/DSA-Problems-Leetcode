35b5aface45125398fd252ef2f0fefaae5a3e2e5
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-02-24 13:36:47 +0530

    Time: 192 ms (61.46%) | Memory: 135.2 MB (25.69%) - LeetSync

--- 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    int find(vector<int>& nums1, vector<int>& nums2, int i, int p1, int p2,vector<vector<int>>&dp,int isSwap){
+    int find(vector<int>& nums1, vector<int>& nums2, bool swapped, int i, vector<vector<int>>&dp){
         if(i==nums1.size()){
             return 0;
         }
-        if(dp[i][isSwap]!=-1)return dp[i][isSwap];
-        int swap = INT_MAX,noSwap=INT_MAX;
-        if(p1<nums2[i] && p2<nums1[i]){
-            swap = 1 + find(nums1, nums2, i+1, nums2[i], nums1[i],dp,1);
+        if(dp[i][swapped]!=-1)return dp[i][swapped];
+        int ans = INT_MAX;
+        int prev1 = nums1[i-1];
+        int prev2 = nums2[i-1];
+        if(swapped){
+            swap(prev1, prev2);
         }
-        if(p1<nums1[i] && p2<nums2[i]){
-            noSwap = find(nums1, nums2, i+1, nums1[i], nums2[i],dp,0);
+        if(nums1[i]>prev1 && nums2[i]>prev2){
+            ans = find(nums1, nums2, 0 , i+1, dp);
         }
-        return dp[i][isSwap] = min(swap, noSwap);
+        if(nums1[i]>prev2 && nums2[i]>prev1){
+            ans = min(ans, 1 + find(nums1, nums2, 1, i+1, dp));
+        }
+        return dp[i][swapped] = ans;
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>>dp(nums1.size(),vector<int>(2,-1));
-        return find(nums1, nums2, 0, -1, -1,dp,0);
+        nums1.insert(nums1.begin(), -1);
+        nums2.insert(nums2.begin(), -1);
+        vector<vector<int>>dp(nums1.size(), vector<int>(2, -1));
+        return find(nums1, nums2, 0, 1, dp);
     }
 };
