11ce1c50d79f017e6ab3f25684bb979ea4d987df
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-02-28 13:31:42 +0530

    Time: 342 ms (15.13%) | Memory: 131.6 MB (10.09%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
+    vector<int> find(vector<int> nums){
+        map<int, int>m;
+        for(auto i:nums){
+            m[i]++;
+        }
+        nums.erase(nums.begin(), nums.end());
+        for(auto i:m){ 
+            while(i.second>0){
+                nums.push_back(i.first);
+                i.second--;
+            }
+        }
+        return nums;
+    }
     int maximumGap(vector<int>& nums) {
         int n = nums.size();
         if(n<2)return 0;
-        sort(nums.begin(), nums.end());
+        nums = find(nums);
         int ans = 0;
         for(int i=0; i<n-1; i++){
             ans = max(ans, nums[i+1] - nums[i]);
