85956969a4c321d6e135ac7a59e1dd847113cb4a
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-03-01 22:33:46 +0530

    Time: 2275 ms (5.00%) | Memory: 654.7 MB (5.00%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool check(string &s){
+        vector<int>v(26, 0);
+        for(auto i:s){
+            v[i-'a']++;
+            if(v[i-'a']>1){
+                return false;
+            }
+        }
+        return true;
+    }
+    int find(int i, vector<string>& arr, string s, unordered_map<string, int>&dp){
+        if(i>=arr.size()){
+            if(check(s)){
+                return s.size();
+            }
+            return -1e9;
+        }
+        // if(dp[i].find(s)!=dp[i].end()){
+        //     return dp[i][s];
+        // }
+        if(dp.find(s)!=dp.end()){
+            return dp[s];
+        }
+        int notTake = find(i+1, arr, s, dp);
+        int take = find(i+1, arr, s+arr[i], dp);
+        return dp[s] = max(take, notTake);
+    }
+    int maxLength(vector<string>& arr) {
+        // map<int, map<string, int>>dp;
+        unordered_map<string, int>dp;
+        return find(0, arr, "", dp);
+    }
+};
