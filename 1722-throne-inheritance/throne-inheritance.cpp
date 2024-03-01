54620f9c8ab7b635cdc4d9d6b28a0e4f4753442d
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-03-01 16:18:59 +0530

    Time: 682 ms (9.87%) | Memory: 184.3 MB (29.83%) - LeetSync

---
+++ 1722-throne-inheritance/throne-inheritance.cpp
@@ -0,0 +1,37 @@
+class ThroneInheritance {
+public:
+    map<string, vector<string>>mp;
+    map<string, int>dead;
+    string king;
+    ThroneInheritance(string kingName) {
+        king = kingName;
+    }
+    void birth(string parentName, string childName) {
+        mp[parentName].push_back(childName);
+    }
+    
+    void death(string name) {
+        dead[name] = 1;
+    }
+    void find(string name, vector<string>&ans){
+        if(dead[name]==0){
+            ans.push_back(name);
+        }
+        for(auto i:mp[name]){
+            find(i, ans);
+        }
+    }
+    vector<string> getInheritanceOrder() {
+        vector<string>ans;
+        find(king, ans);
+        return ans;
+    }
+};
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance* obj = new ThroneInheritance(kingName);
+ * obj->birth(parentName,childName);
+ * obj->death(name);
+ * vector<string> param_3 = obj->getInheritanceOrder();
+ */
