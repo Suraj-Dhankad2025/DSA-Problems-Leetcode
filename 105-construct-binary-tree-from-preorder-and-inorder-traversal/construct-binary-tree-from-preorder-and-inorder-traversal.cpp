e8a9171c5dff27c888c1981939a4bbc91e58c272
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-03-01 23:20:50 +0530

    Time: 21 ms (29.01%) | Memory: 25.5 MB (86.44%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,23 +11,24 @@
  */
 class Solution {
 public:
-    TreeNode* find(vector<int>&pre, int preS, int preE, vector<int>&in, int inS, int inE, map<int, int>&m){
-        if(preS>preE||inS>inE){
-            return NULL;
+    int findNode(vector<int>& in, int s, int e, int t){
+        for(int i=s; i<=e; i++){
+            if(in[i]==t){
+                return i;
+            }
         }
-        TreeNode* root = new TreeNode(pre[preS]);
-        int inRoot = m[pre[preS]];
-        int numLeft = inRoot - inS;
-        root->left = find(pre, preS+1, preS + numLeft, in, inS, inRoot-1, m);
-        root->right = find(pre, preS + numLeft+1, preE, in, inRoot+1, inE, m);
+        return -1;
+    }
+    TreeNode* find(int ind, int s, int e, vector<int>& pre, vector<int>& in){
+        if(s>e)return NULL;
+        TreeNode* root = new TreeNode(pre[ind]);
+        int pos = findNode(in, s, e, pre[ind]);
+        root->left = find(ind+1, s, pos-1, pre, in);
+        root->right = find(ind+(pos-s+1), pos+1, e, pre, in);
         return root;
     }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int,int>m;
-        for(int i=0; i<inorder.size(); i++){
-            m[inorder[i]] = i;
-        }
-        TreeNode* root = find(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, m);
-        return root;
+    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+        int n = pre.size();
+        return find(0, 0, n-1, pre, in);
     }
 };
