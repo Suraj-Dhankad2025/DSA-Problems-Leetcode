d92393aaca2f6336ae4fdf83e06d6a6e7f09a638
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-03-22 08:52:42 +0530

    Time: 10 ms (61.05%) | Memory: 14.4 MB (34.11%) - LeetSync

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    
+    long long find(int i, int steps, int l, vector<vector<long long>>& dp){
+        if(i < 0 || i >= l || steps < 0){
+            return 0;
+        }
+        if(steps == 0){
+            if(i == 0){
+                return 1;
+            } 
+            return 0;
+        }
+        if(dp[i][steps] != -1) return dp[i][steps];
+        
+        long long left = find(i-1, steps-1, l, dp) % mod;
+        long long right = find(i+1, steps-1, l, dp) % mod;
+        long long same = find(i, steps-1, l, dp) % mod;
+        long long ans = (left + right + same) % mod;
+        
+        dp[i][steps] = ans;
+        return ans;
+    }
+    
+    int numWays(int steps, int arrLen) {  
+        int n = min(steps/2 + 1, arrLen);
+        vector<vector<long long>> dp(n+1, vector<long long>(steps+1, -1));
+        return static_cast<int>(find(0, steps, n, dp));
+    }
+};
