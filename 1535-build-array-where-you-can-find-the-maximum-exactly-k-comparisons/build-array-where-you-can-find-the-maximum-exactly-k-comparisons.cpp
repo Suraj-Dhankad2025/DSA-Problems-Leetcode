256969563fc04b4efb8e1479cc4431a0fdc1a4a4
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-03-22 23:04:56 +0530

    Time: 220 ms (17.11%) | Memory: 11.9 MB (50.52%) - LeetSync

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-   int mod = 1000000007;
-
-int find(int i, int m, int k, int prev, vector<vector<vector<int>>>& dp) {
-    if (i < 0) {
-        if (k == 0) {
-            return 1;
+    int mod=1e9+7;
+    int find(int i, int m, int k, int prev, vector<vector<vector<int>>>& dp) {
+        if (i < 0) {
+            if (k == 0) {
+                return 1;
+            }
+            return 0;
         }
-        return 0;
-    }
-    if (k < 0) return 0;
-    if (dp[i][k][prev] != -1) return dp[i][k][prev];
-    
-    long long ans = 0;  // Use long long for intermediate calculations
-    for (int x = 1; x <= m; x++) {
-        if (x > prev) {
-            ans += find(i - 1, m, k - 1, x, dp);
-        } else {
-            ans += find(i - 1, m, k, prev, dp);
+        if (k < 0) return 0;
+        if (dp[i][k][prev] != -1) return dp[i][k][prev];
+        int ans = 0;
+        for (int x = 1; x <= m; x++) {
+            if (x > prev) {
+                ans += find(i - 1, m, k - 1, x, dp)%mod;
+            } else {
+                ans += find(i - 1, m, k, prev, dp)%mod;
+            }
+            ans = ans%mod;
         }
-        ans %= mod;  // Apply modulo operation to avoid overflow
+        return dp[i][k][prev] = ans%mod;
     }
-    return dp[i][k][prev] = ans % mod;
-}
-
-int numOfArrays(int n, int m, int k) {
-    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k + 1, vector<int>(m + 1, -1)));
-    return find(n - 1, m, k, 0, dp);
-}
-
+    int numOfArrays(int n, int m, int k) {
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k + 1, vector<int>(m + 1, -1)));
+        return find(n - 1, m, k, 0, dp);
+    }
 };
