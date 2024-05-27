54d9931e038f190169f50b782f13e4851394d6a6
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-05-27 18:29:34 +0530

    Time: 153 ms (39.01%) | Memory: 74.3 MB (36.59%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
+    bool dfs(int node, vector<int>adj[], vector<int>&color){
+        for(auto i:adj[node]){
+            if(color[i]==-1){
+                color[i] = (color[node]+1)%2;
+                if(dfs(i, adj, color)==0){
+                    return 0;
+                }
+            }
+            else if(color[i]==color[node]){
+                return 0;
+            }
+        }
+        return 1;
+    }
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        queue<int>q;
-        vector<int>color(n+1, -1);
-        
         vector<int>adj[n+1];
         for(auto i:dislikes){
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
+
+        vector<int>color(n+1, -1);
         for(int i=1; i<=n; i++){
             if(color[i]==-1){
                 color[i] = 0;
-                q.push(i);
-                while(!q.empty()){
-                    int node = q.front();
-                    q.pop();
-                    for(auto i:adj[node]){
-                        if(color[i]==-1){
-                            color[i] = (1+color[node])%2;
-                            q.push(i);
-                        }
-                        else if(color[i]==color[node]){
-                            return 0;
-                        }
-                    }
+                if(dfs(i, adj, color)==0){
+                    return 0;
                 }
             }
         }
-        
         return 1;
     }
 };
