e1b85d261d056604c05e37d50c3e126cc04e2b92
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-06-03 08:42:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (40.00%) - LeetSync

--- 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int find(int chair, string s){
+    int minimumChairs(string s) {
+        int ans = INT_MIN;
+        int x=0;
         for(auto i:s){
             if(i=='E'){
-                chair--;
-            }
-            if(i=='L'){
-                chair++;
-            }
-            if(chair<0){
-                return 0;
-            }
-        }
-        return 1;
-    }
-    int minimumChairs(string st) {
-        int s=0,e=st.size();
-        int ans = 0;
-        while(s<e){
-            int mid = s + (e-s)/2;
-            if(find(mid, st)==1){
-                e = mid;
+                x++;
             }
             else{
-                s = mid+1;
+                x--;
             }
+            ans = max(ans, x);
         }
-        return s;
+        return ans;
     }
 };
