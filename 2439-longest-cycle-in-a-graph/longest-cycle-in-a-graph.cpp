cedf505f62a331de0cac1c121d6b663e034458b3
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-06-07 17:30:58 +0530

    Time: 244 ms (59.26%) | Memory: 143.3 MB (37.49%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
- void solve(int node,vector<int>&path,vector<int>&vis,vector<int>adj[],int temp,int &ans)
-    {
+    int maxi=-1;
+    void dfs(vector<int>adj[],vector<int>&vis,vector<int>&path, int node,int dis){
         vis[node]=1;
-        path[node]=temp;
-        for(auto it:adj[node])
-        {
-            if(vis[it]==0) solve(it,path,vis,adj,temp+1,ans);
-            else if(path[it]!=0) ans=max(ans,temp-path[it]+1);
+        path[node] = dis;
+        for(auto it:adj[node]){
+            if(vis[it]==0){
+                dfs(adj,vis,path,it,dis+1);
+            }
+            else if(path[it]!=0){
+                maxi=max(maxi,dis-path[it]+1);
+            }
         }
         path[node]=0;
     }
     int longestCycle(vector<int>& edges) {
         int n=edges.size();
-        vector<int>adj[n];
-        for(int i=0;i<n;i++)
-        {
+        vector<int>adj[n+1];
+        for(int i=0;i<n;i++){
             if(edges[i]!=-1)
-                adj[i].push_back(edges[i]);
+            adj[edges[i]].push_back(i);
         }
-        vector<int>vis(n,0);
-        vector<int>path(n,0);
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                solve(i,path,vis,adj,1,ans);
+        vector<int>vis(n+1,0);
+        vector<int>path(n+1,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(adj,vis,path,i,1);
             }
         }
-        if(ans==0) return -1;
-        return ans;
+        return maxi;
     }
 };
