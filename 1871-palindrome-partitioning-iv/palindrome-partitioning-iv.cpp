2c33e03d3a7d9b4188e85983dafe06e814bce762
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2024-06-08 21:27:54 +0530

    Time: 2118 ms (19.88%) | Memory: 11.4 MB (82.13%) - LeetSync

--- 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.cpp
+++ 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.cpp
@@ -1,25 +1,40 @@
 class Solution {
-public:
-    bool check(string &s, int i, int j){
+    bool ispalin(string &s,int i,int j){
         while(i<j){
-            if(s[i++]!=s[j--])return 0;
+            if(s[i]!=s[j]){
+                return 0;
+            }
+            i++;
+            j--;
         }
         return 1;
     }
-    bool find(int l, int r, string &s,int ans, vector<vector<int>>&dp){
-        if(ans==2){
-            return check(s, l, r);
+    int palin(string &s,int i,int k,vector<vector<int>>&dp){
+        if(i>=s.size()){
+            if(k>0)return 1e5;
+            return 0;
         }
-        if(dp[l][r]!=-1)return dp[l][r];
-        for(int j=l; j<r; j++){
-            if(check(s, l, j) && find(j+1, r, s, ans+1, dp)){
-                return dp[l][r] = 1;
+        if(k==0){
+            return 1e5;
+        }
+        if(dp[i][k]!=-1){
+            return dp[i][k];
+        }
+        int take=1e5;
+        for(int j=i;j<s.size();j++){
+            bool cnt=ispalin(s,i,j);
+            if(cnt){
+                take=min(take,palin(s,j+1,k-1,dp));
             }
         }
-        return dp[l][r] = 0;
+        return dp[i][k]= take;
     }
+public:
     bool checkPartitioning(string s) {
-        vector<vector<int>>dp(s.size(), vector<int>(s.size(), -1));
-        return find(0, s.size()-1, s, 0, dp); 
+        int k=3;
+        vector<vector<int>>dp(s.size(),vector<int>(k+1,-1));
+        int x= palin(s,0,k,dp);
+        if(x>=1e5)return 0;
+        return 1;
     }
 };
